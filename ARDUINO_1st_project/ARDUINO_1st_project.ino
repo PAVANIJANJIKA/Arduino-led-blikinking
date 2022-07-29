2266a3d29fb8db34b2387e684a70e91bedc1c4d6
PAVANIJANJIKA <110219104+PAVANIJANJIKA@users.noreply.github.com>
2022-07-29 11:45:12 +0530

    Add files via upload

---
+++ ARDUINO_1st_project/ARDUINO_1st_project.ino
@@ -0,0 +1,76 @@
+int led1=13; 
+int led2=12;
+int led3=11;
+int led4=10;
+int led5=9;
+int led6=8;
+int led7=7;
+
+void setup() {
+pinMode(13,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(10,OUTPUT);  
+pinMode(9,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(7,OUTPUT);  
+
+}
+
+void loop() {
+digitalWrite(led1,HIGH);
+delay(50);
+digitalWrite(led1,LOW);
+delay(50);
+digitalWrite(led2,HIGH);
+delay(50);
+digitalWrite(led2,LOW);
+delay(50);
+digitalWrite(led3,HIGH);
+delay(50);
+digitalWrite(led3,LOW);
+delay(50);
+digitalWrite(led4,HIGH);
+delay(50);
+digitalWrite(led4,LOW);
+delay(50);
+digitalWrite(led5,HIGH);
+delay(50);
+digitalWrite(led5,LOW);
+delay(50);
+digitalWrite(led6,HIGH);
+delay(50);
+digitalWrite(led6,LOW);
+delay(50);
+digitalWrite(led7,HIGH);
+delay(50);
+digitalWrite(led7,LOW);
+delay(50);
+digitalWrite(led6,HIGH);
+delay(50);
+digitalWrite(led6,LOW);
+delay(50);
+digitalWrite(led5,HIGH);
+delay(50);
+digitalWrite(led5,LOW);
+delay(50);
+digitalWrite(led4,HIGH);
+delay(50);
+digitalWrite(led4,LOW);
+delay(50);
+digitalWrite(led3,HIGH);
+delay(500);
+digitalWrite(led3,LOW);
+delay(50);
+digitalWrite(led2,HIGH);
+delay(50);
+digitalWrite(led2,LOW);
+delay(50);
+digitalWrite(led1,HIGH);
+delay(50);
+digitalWrite(led1,LOW);
+delay(50);
+
+
+
+}
